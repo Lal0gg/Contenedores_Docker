0dcd5dc746a86383c03c3c08dd11c7cfb0a08879
William58-0 <2797652900101@ingenieria.usac.edu.gt>
2022-03-15 00:48:02 -0600

    funciona server en windows

--- server/router/app.js
+++ server/router/app.js
@@ -8,7 +8,7 @@ var mysql = require('mysql');
 // host,user,password,database debe de ser mysql en docker, cambiar 
 /* 
 CREATE TABLE usuario(
-	id int auto_increment,
+    id int auto_increment,
     nombre varchar(50),
     edad int default 18,
     genero varchar(20),
@@ -17,11 +17,11 @@ CREATE TABLE usuario(
     PRIMARY KEY(id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 */
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'Kevin',
-    password: '741236987',
-    database: 'pruebas'
+const connection = mysql.createConnection({
+    host: 'host.docker.internal',
+    user: 'root',
+    password: 'pass',
+    database: 'emails_db'
 });
 
 const app = express();
@@ -33,98 +33,112 @@ app.use(bodyparser.urlencoded({
 }));
 
 
-app.get('/hola',(req,res) => {
-    res.send('87');
+app.get('/hola', (req, res) => {
+    const sqlQuery = 'CREATE TABLE IF NOT EXISTS usuario(\
+        id int auto_increment,\
+        nombre varchar(50),\
+        edad int default 18,\
+        genero varchar(20),\
+        correo varchar(100) not null unique,\
+        adicional varchar(100),\
+        PRIMARY KEY(id))';
+
+    connection.query(sqlQuery, (err) => {
+        if (err) throw err;
+
+        res.send('Table created!')
+    });
 });
 
+app.post('/insertar', (req, res) => {
 
-app.post('/insertar',(req,res) =>{
- 
-    connection.query('INSERT INTO usuario(nombre,edad,correo,genero,adicional) VALUES(? , ?, ?, ?,?);',[req.body.nombre,req.body.edad,req.body.correo,req.body.genero,req.body.adicional],function (error, results, fields){
-        if(error){
+    connection.query('INSERT INTO usuario(nombre,edad,correo,genero,adicional) VALUES(? , ?, ?, ?,?);', [req.body.nombre, req.body.edad, req.body.correo, req.body.genero, req.body.adicional], function (error, results, fields) {
+        if (error) {
             console.log(error);
-            res.setHeader('Access-Control-Allow-Headers','Content-Type');
-            res.setHeader('Access-Control-Allow-Mathods','GET,PUT,POST,DELETE,OPTIONS');
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+            res.setHeader('Access-Control-Allow-Mathods', 'GET,PUT,POST,DELETE,OPTIONS');
             res.statusCode = 500;
             res.contentType('application/json').json({
                 sqlmessage: error.sqlMessage,
                 result: 'No se pudo insertar',
                 codigo: 500
             });
-        
-        }else{
+
+        } else {
             console.log(results);
-            res.setHeader('Access-Control-Allow-Headers','Content-Type');
-            res.setHeader('Access-Control-Allow-Mathods','GET,PUT,POST,DELETE,OPTIONS');
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+            res.setHeader('Access-Control-Allow-Mathods', 'GET,PUT,POST,DELETE,OPTIONS');
             res.statusCode = 200;
             res.contentType('application/json').json({
                 result: 'Si se inserto',
                 codigo: 200
             });
-        
+
         }
-       
-    }); 
-    
+
+    });
+
 });
 
-app.get('/get_ids',(req,res) =>{
-    
-    connection.query('SELECT  id FROM usuario;',function (error,results,fields){
-        if(error){
-            res.setHeader('Access-Control-Allow-Headers','Content-Type');
-            res.setHeader('Access-Control-Allow-Mathods','GET,PUT,POST,DELETE,OPTIONS');
+app.get('/get_ids', (req, res) => {
+
+    connection.query('SELECT  id FROM usuario;', function (error, results, fields) {
+        if (error) {
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+            res.setHeader('Access-Control-Allow-Mathods', 'GET,PUT,POST,DELETE,OPTIONS');
             res.statusCode = 500;
             res.contentType('application/json').json({
                 result: 'No se pudo obtener los ids'
             });
-        
-        }else{
+
+        } else {
             console.log(results);
-            res.setHeader('Access-Control-Allow-Headers','Content-Type');
-            res.setHeader('Access-Control-Allow-Mathods','GET,PUT,POST,DELETE,OPTIONS');
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+            res.setHeader('Access-Control-Allow-Mathods', 'GET,PUT,POST,DELETE,OPTIONS');
             res.statusCode = 200;
             res.contentType('application/json').json({
                 result: results
             });
         }
-        
+
     });
 
 });
 
-app.post('/get_user',(req,res) =>{
- //   connection.connect();
-    connection.query('SELECT nombre,edad,genero,correo,adicional FROM usuario where id = ?;',[req.body.id], function(error,results,fields){
-        if(error){
-            res.setHeader('Access-Control-Allow-Headers','Content-Type');
-            res.setHeader('Access-Control-Allow-Mathods','GET,PUT,POST,DELETE,OPTIONS');
+app.post('/get_user', (req, res) => {
+    //   connection.connect();
+    connection.query('SELECT nombre,edad,genero,correo,adicional FROM usuario where id = ?;', [req.body.id], function (error, results, fields) {
+        if (error) {
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+            res.setHeader('Access-Control-Allow-Mathods', 'GET,PUT,POST,DELETE,OPTIONS');
             res.statusCode = 500;
             res.contentType('application/json').json({
                 result: 'error id'
             });
-        
+
         }
-        else{
+        else {
             console.log(results);
-            res.setHeader('Access-Control-Allow-Headers','Content-Type');
-            res.setHeader('Access-Control-Allow-Mathods','GET,PUT,POST,DELETE,OPTIONS');
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+            res.setHeader('Access-Control-Allow-Mathods', 'GET,PUT,POST,DELETE,OPTIONS');
             res.statusCode = 200;
             res.contentType('application/json').json(results
             );
         }
-   //     connection.end();
+        //     connection.end();
     });
-    
+
 });
 
-app.get('/gettodos',(req,res) =>{
-    connection.query('SELECT * FROM usuario',function(error,results,fields){
-        if(error){
+app.get('/gettodos', (req, res) => {
+    //const sqlQuery = 'SELECT * FROM usuario';
 
-        }else{
-            res.setHeader('Access-Control-Allow-Headers','Content-Type');
-            res.setHeader('Access-Control-Allow-Mathods','GET,PUT,POST,DELETE,OPTIONS');
+    connection.query('SELECT * FROM usuario', function (error, results, fields) {
+        if (error) {
+
+        } else {
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+            res.setHeader('Access-Control-Allow-Mathods', 'GET,PUT,POST,DELETE,OPTIONS');
             res.statusCode = 200;
             res.contentType('application/json').json({
                 result: results
@@ -132,68 +146,69 @@ app.get('/gettodos',(req,res) =>{
         }
     });
 });
-app.post('/update',(req,res)=>{
-   // connection.connect();
-    connection.query('UPDATE usuario SET nombre = ?, edad = ?, genero= ?, correo = ?, adicional = ?  WHERE id = ?',[req.body.nombre,req.body.edad,req.body.genero,req.body.correo,req.body.adicional,req.body.id], function(error,results,fields){
-        if(error){
-            res.setHeader('Access-Control-Allow-Headers','Content-Type');
-            res.setHeader('Access-Control-Allow-Mathods','GET,PUT,POST,DELETE,OPTIONS');
+
+app.post('/update', (req, res) => {
+    // connection.connect();
+    connection.query('UPDATE usuario SET nombre = ?, edad = ?, genero= ?, correo = ?, adicional = ?  WHERE id = ?', [req.body.nombre, req.body.edad, req.body.genero, req.body.correo, req.body.adicional, req.body.id], function (error, results, fields) {
+        if (error) {
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+            res.setHeader('Access-Control-Allow-Mathods', 'GET,PUT,POST,DELETE,OPTIONS');
             res.statusCode = 500;
             res.contentType('application/json').json({
                 result: 'error id o no se puedo actualizar',
                 codigo: 500
             });
-        
+
         }
-        else{
+        else {
             console.log(results);
-            res.setHeader('Access-Control-Allow-Headers','Content-Type');
-            res.setHeader('Access-Control-Allow-Mathods','GET,PUT,POST,DELETE,OPTIONS');
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+            res.setHeader('Access-Control-Allow-Mathods', 'GET,PUT,POST,DELETE,OPTIONS');
             res.statusCode = 200;
             res.contentType('application/json').json({
                 result: 'Actualizacion correcta',
                 codigo: 200
             });
         }
-      //  connection.end();
+        //  connection.end();
     });
-    
+
 });
 
-app.post('/delete',(req,res) =>{
-    connection.query('DELETE FROM usuario WHERE id = ?',[req.body.id], function(error,results,fields){
-        if(error){
-            res.setHeader('Access-Control-Allow-Headers','Content-Type');
-            res.setHeader('Access-Control-Allow-Mathods','GET,PUT,POST,DELETE,OPTIONS');
+app.post('/delete', (req, res) => {
+    connection.query('DELETE FROM usuario WHERE id = ?', [req.body.id], function (error, results, fields) {
+        if (error) {
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+            res.setHeader('Access-Control-Allow-Mathods', 'GET,PUT,POST,DELETE,OPTIONS');
             res.statusCode = 500;
             res.contentType('application/json').json({
                 result: 'no se pudo eliminar',
                 codigo: 500
             });
-        
+
         }
-        else{
+        else {
             console.log(results);
-            res.setHeader('Access-Control-Allow-Headers','Content-Type');
-            res.setHeader('Access-Control-Allow-Mathods','GET,PUT,POST,DELETE,OPTIONS');
+            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+            res.setHeader('Access-Control-Allow-Mathods', 'GET,PUT,POST,DELETE,OPTIONS');
             res.statusCode = 200;
-            
-            if(results.affectedRows == 0){
+
+            if (results.affectedRows == 0) {
                 res.contentType('application/json').json({
                     result: 'id no existe'
-                }); 
+                });
             }
-            else if(results.affectedRows == 1){
+            else if (results.affectedRows == 1) {
                 res.contentType('application/json').json({
                     result: 'Eliminacion correcta',
                     codigo: 200
-                }); 
+                });
             }
 
-            
-            
+
+
         }
-      //  connection.end();
+        //  connection.end();
     });
 })
 

c9ddf72cc02f2732eef4f71af66a8f482ac1ffdd
Kevin Garcia <2324015000101@ingenieria.usac.edu.gt>
2022-03-14 21:02:18 -0600

    ADD dockerfile
    Se agrego dockerfile para el backend

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@
 const app = require("./router/app");
 
 
-// Direccion port y ip debe ser la docker, cambiar 
-app.listen(3000,'192.168.137.1',()=>{
+// Direccion port debe ser la docker, cambiar 
+app.listen(3000,()=>{
     console.log("Servidor ejecutandose puerto 3000");
 });
